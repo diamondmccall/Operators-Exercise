ed0d8eb5203e4d6797adb7961cb0099624e123ad
Diamond Mccall <singlikediamond@gmail.com>
2023-01-20 20:32:22 -0600

    inital commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //excercise1//
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radis of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(AreaOfCirle(radius));
+        }
+
+        public static double AreaOfCirle(double radius)
+
+
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+            Console.WriteLine("What is the radius of the cirle?");
+
+        }
+
         }
     }
-}
+
